5180a671d21cb181ee5a5bc05ad0aa44a869af39
Abhishek Babu <abhideep0408@gmail.com>
2020-07-27 20:59:49 -0700

    Able to see user data after sign-in

---
+++ app/routes.py
@@ -0,0 +1,74 @@
+from flask import Flask, redirect, render_template, request
+from app import app
+import urllib
+from urllib.parse import quote
+import base64
+import requests
+import json
+
+#  Client keys
+CLIENT_ID = "78475032f6474dc7a7dabf6fc6b9d1cf"
+CLIENT_SECRET = "38d6c17959ed4abaa582503a118488e2"
+
+# Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+# Server-side parameters
+CLIENT_SIDE_URL = "http://127.0.0.1:5000/"
+REDIRECT_URI = "http://localhost:5000/visualize"
+SCOPE = 'user-read-private user-read-playback-state user-modify-playback-state user-library-read'
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    "client_id": CLIENT_ID
+}
+
+# App routes
+@app.route('/')
+def spotify():
+    return render_template('login.html')
+
+@app.route('/authenticate')
+def spotify_auth():
+    url_args = "&".join(["{}={}".format(key,urllib.parse.quote(val)) for key, val in auth_query_parameters.items()])
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    return redirect(auth_url)
+ 
+@app.route('/visualize')
+def spotify_sentiment_analysis():
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI
+    }
+
+    base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET).encode())
+    headers = {"Authorization": "Basic {}".format(base64encoded.decode())}
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
+
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    token_type = response_data["token_type"]
+    expires_in = response_data["expires_in"]
+
+    authorization_header = {"Authorization":"Bearer {}".format(access_token)}
+
+    # Get profile data
+    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    profile_data = json.loads(profile_response.text)
+    
+    print(profile_data)
+
+    return render_template('visualize.html')
